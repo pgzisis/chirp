9b240ac71e5099def666e47ab11546c0bbe61727
pgzisis <zisispg@gmail.com>
2024-04-04 02:23:54 +0200

    Add styling, CreatePostWizard, PostView

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,44 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import type { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => ({
+  id: user.id,
+  username: user.username,
+  imageUrl: user.imageUrl,
+});
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author?.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
